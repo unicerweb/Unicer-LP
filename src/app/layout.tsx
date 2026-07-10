b94e4addccfd73ac6e47a6853764790cda8f6bb8
unicerweb <unicer.web@gmail.com>
2026-07-10 13:51:15 -0300

    Instalação Pixel Meta Unicer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,28 @@ export default function RootLayout({
 }>) {
   return (
         <html lang="pt-BR" className={`${dmSans.variable} h-full scroll-smooth`}>
-        <body className="min-h-full bg-white antialiased">
+        <body className="min-h-full bg-white antialiased"><Script id="meta-pixel-unicer" strategy="afterInteractive">
+  {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1057442187036899');
+    fbq('track', 'PageView');
+  `}
+</Script>
+<noscript>
+  <img
+    height="1"
+    width="1"
+    style={{ display: "none" }}
+    src="https://www.facebook.com/tr?id=1057442187036899&ev=PageView&noscript=1"
+  />
+</noscript>
           <Script
             src="https://www.googletagmanager.com/gtag/js?id=AW-18303231984"
             strategy="afterInteractive"
